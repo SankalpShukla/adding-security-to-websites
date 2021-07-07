f85da3309b4dece438ce70fe52d35c36d29afe74
Sankalp Shukla <sankalpshukla@Sankalps-MacBook-Air.local>
2021-07-07 16:35:31 +0530

    Level 3 (Hashing)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const { urlencoded } = require("body-parser")
 const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
+const md5 = require('md5')
 
 const app = express()
 
@@ -19,11 +19,6 @@ userSchema = new mongoose.Schema({
     email : String,
     password : String
 })
-
-//level 2:
- //can be any string
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]}); //this should be done before making the mongoose model. Now this encrypts both email and password but we just need to encrypt the password so.. 
-
 
 const User = new mongoose.model("User",userSchema)
 
@@ -42,7 +37,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password) //level3
     })
 
     newUser.save(function(err){
@@ -57,7 +52,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email:username},function(err,foundUser){ //level 1
         if(err)
